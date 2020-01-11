c3abcbf200b97037423bb6a7e49133d8611b656a
Josh Long <josh@joshlong.com>
2020-01-11 15:50:03 -0800

    Up

--- src/main/java/generator/SiteGeneratorApplication.java
+++ src/main/java/generator/SiteGeneratorApplication.java
@@ -4,12 +4,19 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is a giant Spring Batch {@link org.springframework.batch.core.Job job} that I use
+ * to generate the web site for the <a href="http://bootifulpodcast.fm">podcast's
+ * website</a>
+ *
+ * @author Josh Long
+ */
 @EnableBatchProcessing
 @SpringBootApplication
-public class GeneratorApplication {
+public class SiteGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GeneratorApplication.class, args);
+		SpringApplication.run(SiteGeneratorApplication.class, args);
 	}
 
 }
