e07232c4d2620ccda36ae31a10984a9afac98d6d
Josh Long <josh@joshlong.com>
2020-01-12 19:10:07 -0800

    Adding a separate script to run the task.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,3 +7,6 @@ cf push --no-start -u process --no-route -p target/${APP_NAME}.jar ${APP_NAME}
 cf set-env ${APP_NAME} GIT_PASSWORD $GIT_PASSWORD
 cf set-env ${APP_NAME} GIT_USERNAME $GIT_USERNAME
 cf set-env ${APP_NAME} GIT_URI $GIT_URI
+
+
+./run-task.sh
--- deploy/run-task.sh
+++ deploy/run-task.sh
@@ -4,4 +4,4 @@ export CF_TRACE=true
 export APP_NAME=site-generator
 export TASK_NAME=${APP_NAME}-task
 echo "about to run the task, ${APP_NAME} with task name ${TASK_NAME}."
-cf run-task ${APP-NAME} ".java-buildpack/open_jdk_jre/bin/java cp . org.springframework.boot.loader.JarLauncher" # --name ${TASK_NAME}
+./cf-osx run-task ${APP-NAME} ".java-buildpack/open_jdk_jre/bin/java cp . org.springframework.boot.loader.JarLauncher" # --name ${TASK_NAME}
