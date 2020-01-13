eced0130b1580d3134ce647dcb01388e8e72a703
Josh Long <josh@joshlong.com>
2020-01-12 18:41:32 -0800

    fixing cf.sh

--- deploy/cf.sh
+++ deploy/cf.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p $HOME/bin
-cp ./deploy/cf-6.38.0 $HOME/bin/cf
+cp ./deploy/cf-linux $HOME/bin/cf
 cf login  -u $CF_USER -a $CF_API -o $CF_ORG -p $CF_PASSWORD -s $CF_SPACE
-cf install-plugin -f ./deploy/scheduler-for-pcf-cliplugin-linux64-binary-1.1.0
 
 
 
