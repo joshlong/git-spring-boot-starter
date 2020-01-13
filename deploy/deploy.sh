3022e8dd5f677f3205c9316645d89310c6207673
Josh Long <josh@joshlong.com>
2020-01-12 16:10:54 -0800

    deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,9 +2,7 @@
 
 APP_NAME=site-generator-job
 JOB_NAME=$APP_NAME
-
 SCHEDULER_NAME=scheduler-bootiful-podcast
-
 
 cf push -b java_buildpack -u none --no-route --no-start -p target/${APP_NAME}.jar ${APP_NAME}
 cf set-health-check $APP_NAME none # the new version of the cf cli will take 'process' instead of 'none'
