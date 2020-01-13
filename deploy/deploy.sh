311a07c0c4de9f2a64eecbef7ae7e4ba632bc6ca
Josh Long <josh@joshlong.com>
2020-01-12 18:39:22 -0800

    adding the $GIT_URI environment variable.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,6 +6,7 @@ export TASK_NAME=${APP_NAME}-task
 cf push --no-start -u process --no-route -p target/${APP_NAME}.jar ${APP_NAME}
 cf set-env ${APP_NAME} GIT_PASSWORD $GIT_PASSWORD
 cf set-env ${APP_NAME} GIT_USERNAME $GIT_USERNAME
+cf set-env ${APP_NAME} GIT_URI $GIT_URI
 cf restart ${APP_NAME}
 echo "about to run the task..."
 cf run-task ${APP-NAME} ".java-buildpack/open_jdk_jre/bin/java cp . org.springframework.boot.loader.JarLauncher" --name ${APP_NAME}
