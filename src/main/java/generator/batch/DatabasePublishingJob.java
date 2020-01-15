24862052177ec11228169664632003f219ea0848
Josh Long <josh@joshlong.com>
2020-01-15 03:23:20 -0800

    reuse the directory initializer.

--- src/main/java/generator/batch/DatabasePublishingJob.java
+++ src/main/java/generator/batch/DatabasePublishingJob.java
@@ -18,12 +18,13 @@ class DatabasePublishingJob {
 	}
 
 	@Bean
-	Job job(JobBuilderFactory jobBuilderFactory, Step1Configuration s1,
-			Step2Configuration s2, Step3Configuration s3, Step4Configuration s4) {
+	Job job(JobBuilderFactory jobBuilderFactory, Step0Configuration s0,
+			Step1Configuration s1, Step2Configuration s2, Step3Configuration s3,
+			Step4Configuration s4) {
 		return jobBuilderFactory //
 				.get("podcast-publishing-job") //
 				.incrementer(new RunIdIncrementer()) //
-				.start(s1.readPodcastsIntoDescriptions())//
+				.start(s0.reset()).next(s1.readPodcastsIntoDescriptions())//
 				.next(s2.readDescriptionsIntoPages())//
 				.next(s3.copyFilesIntoPlace())//
 				.next(s4.commitPagesToGithub())//
---
+++ src/main/java/generator/batch/Step0Configuration.java
@@ -0,0 +1,44 @@
+package generator.batch;
+
+import generator.FileUtils;
+import generator.SiteGeneratorProperties;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.util.stream.Stream;
+
+@Configuration
+@RequiredArgsConstructor
+@Log4j2
+class Step0Configuration {
+
+	private final SiteGeneratorProperties properties;
+
+	private final StepBuilderFactory stepBuilderFactory;
+
+	private static String NAME = "reset";
+
+	@Bean
+	Step reset() {
+		return this.stepBuilderFactory.get(NAME)
+				.tasklet((stepContribution, chunkContext) -> {
+					var output = properties.getOutput();
+					Stream.of(output.getItems(), output.getPages())
+							.forEach(Step0Configuration.this::reset);
+					return RepeatStatus.FINISHED;
+				}).build();
+	}
+
+	private void reset(File file) {
+		log.info("resetting the directory " + file.getAbsolutePath());
+		FileUtils.delete(file);
+		FileUtils.ensureDirectoryExists(file);
+	}
+
+}
