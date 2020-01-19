dbeaf85bc35b42562c17ce21fb1f5dc3c52b6612
Josh Long <josh@joshlong.com>
2020-01-18 23:04:09 -0800

    site generation ftw.

--- src/main/java/generator/SiteGeneratorApplication.java
+++ src/main/java/generator/SiteGeneratorApplication.java
@@ -12,69 +12,22 @@ import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 
 /**
- * This is a giant Spring Batch {@link org.springframework.batch.core.Job job} that I use
- * to generate the web site for the <a href="http://bootifulpodcast.fm">podcast's
- * website</a>.
- * <p>
  * The job runs both when the application starts up <em>and</em> when requests come via
  * the RabbitMQ queue defined in
  * {@link SiteGeneratorProperties.Launcher#getRequestsQueue()}.
  */
 @Log4j2
-//@EnableBatchProcessing
 @EnableConfigurationProperties(SiteGeneratorProperties.class)
 @SpringBootApplication
 @RequiredArgsConstructor
 public class SiteGeneratorApplication {
 
-//	private final JobLauncher jobLauncher;
-//
-//	private final Job job;
+	public static void main(String[] args) {
 
-  // todo restore the MQ
-////	@Bean
-//  IntegrationFlow integrationFlow(RabbitMqHelper helper,
-//                                  ConnectionFactory connectionFactory, SiteGeneratorProperties properties) {
-//
-//    helper.defineDestination(properties.getLauncher().getRequestsQueue(),
-//        properties.getLauncher().getRequestsExchange(),
-//        properties.getLauncher().getRequestsRoutingKey());
-//
-//    var amqpInboundAdapter = Amqp//
-//        .inboundAdapter(connectionFactory,
-//            properties.getLauncher().getRequestsQueue())//
-//        .get();
-//
-//    return IntegrationFlows//
-//        .from(amqpInboundAdapter)//
-//        .handle(String.class, (str, messageHeaders) -> {//
-//          var ok = this.launchSiteGeneratorJob();
-//          log.info("the job " + (!ok ? "is not" : "is") + " completed.");
-//          return null;
-//        })//
-//        .get();
-//  }
-
-/*
-	@SneakyThrows
-	private boolean launchSiteGeneratorJob() {
-		var parameters = new JobParametersBuilder()//
-				.addDate("runtime", new Date())//
-				.toJobParameters();
-		var jobExecution = this.jobLauncher.run(this.job, parameters);
-		return jobExecution.getEndTime() != null
-				&& jobExecution.getExitStatus().equals(ExitStatus.COMPLETED);
+		var newArgs = (args.length == 0) ? //
+				new String[] { "runtime=" + System.currentTimeMillis() } : //
+				args;
+		SpringApplication.run(SiteGeneratorApplication.class, newArgs);
 	}
-*/
-
-  public static void main(String[] args) {
-    // this creates a unique argument and thus a
-    // unique `JobParameter` to the provided `JobLauncher`.
-
-    var newArgs = (args.length == 0) ? //
-        new String[]{"runtime=" + System.currentTimeMillis()} : //
-        args;
-    SpringApplication.run(SiteGeneratorApplication.class, newArgs);
-  }
 
 }
--- src/main/java/generator/batch/GeneratorJob.java
+++ src/main/java/generator/batch/GeneratorJob.java
@@ -1,6 +1,7 @@
 package generator.batch;
 
 import generator.DateUtils;
+import generator.FileUtils;
 import generator.SiteGeneratorProperties;
 import generator.git.GitTemplate;
 import generator.templates.MustacheService;
@@ -8,102 +9,176 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.eclipse.jgit.api.Git;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.util.FileCopyUtils;
 
+import javax.sql.DataSource;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.net.URL;
+import java.time.Instant;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-@Configuration
 @Log4j2
-@RequiredArgsConstructor
+@Component
 class GeneratorJob {
 
-  private final JdbcTemplate template;
-  private final PodcastRowMapper podcastRowMapper;
-  private final SiteGeneratorProperties properties;
-  private final MustacheService mustacheService;
-  private final GitTemplate gitTemplate;
+	private final JdbcTemplate template;
 
-  @SneakyThrows
-  private void downloadImageFor(PodcastRecord podcast) {
-    var uid = podcast.getPodcast().getUid();
-    var imagesDirectory = new File(this.properties.getOutput().getPages(), "episode-photos");
-    var profilePhotoUrl = new URL(this.properties.getApiServerUrl().toString() + "/podcasts/" + uid + "/profile-photo");
-    var file = new File(imagesDirectory, uid + ".jpg");
-    try (var fin = profilePhotoUrl.openStream(); var fout = new FileOutputStream(file)) {
-      FileCopyUtils.copy(fin, fout);
-      log.info("the image file lives in " + file.getAbsolutePath());
-    }
-  }
+	private final PodcastRowMapper podcastRowMapper;
 
-  @EventListener(ApplicationReadyEvent.class)
-  public void build() throws Exception {
-    var allPodcasts = this.template.query(this.properties.getSql().getLoadPodcasts(), this.podcastRowMapper)
-        .stream()
-        .map(p -> new PodcastRecord(p, "episode-photos/" + p.getUid() + ".jpg"))
-        .collect(Collectors.toList());
-    allPodcasts.forEach(this::downloadImageFor);
-    allPodcasts.sort((o1, o2) -> {
-      var date1 = o1.getPodcast().getDate();
-      var date2 = o2.getPodcast().getDate();
-      return date1.compareTo(date2);
-    });
-    var top3 = new ArrayList<PodcastRecord>();
-    var max = 3;
-    for (var i = 0; i < max && i < allPodcasts.size(); i++) {
-      var podcastRecord = allPodcasts.get(i);
-      top3.add(podcastRecord);
-      log.info("adding " + podcastRecord.getPodcast().getTitle());
-    }
-    var map = this.podcastsByYear(allPodcasts);
-    var years = new ArrayList<YearRollup>();
-    map.forEach((year, podcasts) -> years.add(new YearRollup(year, podcasts)));
-    years.sort(Comparator.comparing(YearRollup::getYear));
-    var html = this.mustacheService.convertMustacheTemplateToHtml(this.properties.getTemplates().getPageChromeTemplate(), Map.of("top3", top3, "years", years));
-    var page = new File(this.properties.getOutput().getPages(), "episodes.html");
-    try (var fout = new FileWriter(page)) {
-      FileCopyUtils.copy(html, fout);
-    }
-    log.info("wrote the template to " + page.getAbsolutePath());
-  }
+	private final SiteGeneratorProperties properties;
 
+	private final MustacheService mustacheService;
 
-  private Map<Integer, List<PodcastRecord>> podcastsByYear(List<PodcastRecord> podcasts) {
-    var map = new HashMap<Integer, List<PodcastRecord>>();
-    for (var podcast : podcasts) {
-      var calendar = DateUtils.getCalendarFor(podcast.getPodcast().getDate());
-      var year = calendar.get(Calendar.YEAR);
-      if (!map.containsKey(year)) {
-        map.put(year, new ArrayList<>());
-      }
-      map.get(year).add(podcast);
-    }
-    map.forEach((key, value) -> value.sort(Comparator.comparing((Function<PodcastRecord, Date>) podcastRecord -> podcastRecord.getPodcast().getDate()).reversed()));
-    return map;
-  }
+	private final GitTemplate gitTemplate;
+
+	private final Resource staticAssets;
+
+	GeneratorJob(JdbcTemplate template, PodcastRowMapper podcastRowMapper,
+														SiteGeneratorProperties properties, MustacheService mustacheService,
+														GitTemplate gitTemplate, @Value("classpath:/static") Resource staticAssets) {
+		this.template = template;
+		this.staticAssets = staticAssets;
+		this.podcastRowMapper = podcastRowMapper;
+		this.properties = properties;
+		this.mustacheService = mustacheService;
+		this.gitTemplate = gitTemplate;
+	}
+
+	@SneakyThrows
+	private void downloadImageFor(PodcastRecord podcast) {
+		var uid = podcast.getPodcast().getUid();
+		var imagesDirectory = new File(this.properties.getOutput().getPages(),
+			"episode-photos");
+		var profilePhotoUrl = new URL(this.properties.getApiServerUrl().toString()
+			+ "/podcasts/" + uid + "/profile-photo");
+		var file = new File(imagesDirectory, uid + ".jpg");
+		try (var fin = profilePhotoUrl.openStream();
+							var fout = new FileOutputStream(file)) {
+			FileCopyUtils.copy(fin, fout);
+			log.info("the image file lives in " + file.getAbsolutePath());
+		}
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void build() throws Exception {
+		log.info("starting the site generation @ " + DateUtils.dateAndTime().format(new Date()));
+
+		var allPodcasts = this.template
+			.query(this.properties.getSql().getLoadPodcasts(), this.podcastRowMapper)
+			.stream()
+			.map(p -> new PodcastRecord(p, "episode-photos/" + p.getUid() + ".jpg"))
+			.collect(Collectors.toList());
+		allPodcasts.forEach(this::downloadImageFor);
+		allPodcasts.sort((o1, o2) -> {
+			var date1 = o1.getPodcast().getDate();
+			var date2 = o2.getPodcast().getDate();
+			return date1.compareTo(date2);
+		});
+
+		// get the top3 latest episodes
+		var top3 = new ArrayList<PodcastRecord>();
+		for (var i = 0; i < 3 && i < allPodcasts.size(); i++) {
+			top3.add(allPodcasts.get(i));
+		}
+
+		var map = this.getPodcastsByYear(allPodcasts);
+		var years = new ArrayList<YearRollup>();
+		map.forEach((year, podcasts) -> years.add(new YearRollup(year, podcasts)));
+		years.sort(Comparator.comparing(YearRollup::getYear));
+		var pageChromeTemplate = this.properties.getTemplates().getPageChromeTemplate();
+		var html = this.mustacheService.convertMustacheTemplateToHtml(pageChromeTemplate,
+			Map.of("top3", top3, "years", years));
+		var page = new File(this.properties.getOutput().getPages(), "index.html");
+		try (var fout = new FileWriter(page)) {
+			FileCopyUtils.copy(html, fout);
+		}
+		log.info("wrote the template to " + page.getAbsolutePath());
+
+		copyPagesIntoPlace();
+		commit();
+	}
+
+	@SneakyThrows
+	private void commit() {
+		var gitCloneDirectory = properties.getOutput().getGitClone();
+		var pagesDirectory = properties.getOutput().getPages();
+		this.gitTemplate.executeAndPush(git -> Stream
+			.of(Objects.requireNonNull(pagesDirectory.listFiles()))
+			.map(fileToCopyToGitRepo -> FileUtils.copy(fileToCopyToGitRepo,
+				new File(gitCloneDirectory, fileToCopyToGitRepo.getName())))
+			.forEach(file -> add(git, file)));
+	}
+
+	@SneakyThrows
+	private void add(Git g, File f) {
+		log.info("adding " + f.getAbsolutePath());
+		g.add().addFilepattern(f.getName()).call();
+		g.commit().setMessage("Adding " + f.getName() + " @ " + Instant.now().toString())
+			.call();
+	}
+
+	@SneakyThrows
+	private void copyPagesIntoPlace() {
+
+		var pagesFile = this.properties.getOutput().getPages();
+
+		// copy all the files in /static/* to the output/*
+		Arrays.asList(Objects.requireNonNull(staticAssets.getFile().listFiles())).forEach(
+			file -> FileUtils.copy(file, new File(pagesFile, file.getName())));
+
+	}
+
+	private Map<Integer, List<PodcastRecord>> getPodcastsByYear(
+		List<PodcastRecord> podcasts) {
+		var map = new HashMap<Integer, List<PodcastRecord>>();
+		for (var podcast : podcasts) {
+			var calendar = DateUtils.getCalendarFor(podcast.getPodcast().getDate());
+			var year = calendar.get(Calendar.YEAR);
+			if (!map.containsKey(year)) {
+				map.put(year, new ArrayList<>());
+			}
+			map.get(year).add(podcast);
+		}
+		map.forEach((key,
+															value) -> value.sort(Comparator.comparing(
+			(Function<PodcastRecord, Date>) podcastRecord -> podcastRecord
+				.getPodcast().getDate())
+			.reversed()));
+		return map;
+	}
 
 }
-
 
 @Data
 @RequiredArgsConstructor
 class YearRollup {
-  private final int year;
-  private final Collection<PodcastRecord> episodes;
+
+	private final int year;
+
+	private final Collection<PodcastRecord> episodes;
+
 }
 
 @RequiredArgsConstructor
 @Data
 class PodcastRecord {
-  private final Podcast podcast;
-  private final String imageSrc;
+
+	private final Podcast podcast;
+
+	private final String imageSrc;
+
 }
