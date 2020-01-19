384e2d5f8547417789c748a27b529af9de60ea28
Josh Long <josh@joshlong.com>
2020-01-19 06:22:21 -0800

    polishing this and that.

--- src/main/resources/static/assets/js/custom.js
+++ src/main/resources/static/assets/js/custom.js
@@ -11,6 +11,18 @@ $(document).ready(function () {
 
 
 $(document).ready(function () {
+	$('.tab-pane-toggle').click(function (e) {
+		var id = e.currentTarget.id;
+		var contentDiv = id.split ('-tab')[0] + '-content'
+		console.log ( 'the content div is ' + contentDiv)
+		$('.tab-pane-content').removeClass('active');
+		$('#' + contentDiv).addClass('active');
+	});
+})
+
+
+
+$(document).ready(function () {
 /* var navheight = $('#navbar').height(); */
 
 	$("#scrollTop, .btn-slide").click(function() {
