783874ab8bf493fdf7359aec44df69d583420ddd
Josh Long <josh@joshlong.com>
2020-01-19 06:04:42 -0800

    adding bootstrap.

--- src/main/resources/static/assets/js/custom.js
+++ src/main/resources/static/assets/js/custom.js
@@ -1,37 +1,29 @@
+
+
+
 $(document).ready(function () {
 
-    $('.mobile-nav-btn').on('click', function () {
-        $('.hamburger-menu').toggleClass('open');
-    });
+  $('.mobile-nav-btn').on('click', function () {
+    $('.hamburger-menu').toggleClass('open');
+  });
 
 });
 
-$(document).ready(function () {
-    $('.tab-pane-toggle').click(function (e) {
-        var id = e.currentTarget.id;
-        var contentDiv = id.split ('-tab')[0] + '-content'
-		console.log ( 'the content div is ' + contentDiv)
-		$('.tab-pane-content').removeClass('active');
-		$('#' + contentDiv).addClass('active');
-    });
-})
-
 
 $(document).ready(function () {
-    /* var navheight = $('#navbar').height(); */
+/* var navheight = $('#navbar').height(); */
 
-
-    $("#scrollTop, .btn-slide").click(function () {
-        // get the destination
-        var destination = $(this).attr('href');
-
-        $('html, body').stop().animate({
-            scrollTop: $(destination).offset().top
-        }, 700);
-
-        // override the default link click behavior
-        return false;
-    });
+	$("#scrollTop, .btn-slide").click(function() {
+	    // get the destination
+	    var destination = $(this).attr('href');
+	    
+	    $('html, body').stop().animate({
+	        scrollTop: $(destination).offset().top
+	    }, 700);
+	    
+	    // override the default link click behavior
+	    return false;
+	});
 
 });
 
