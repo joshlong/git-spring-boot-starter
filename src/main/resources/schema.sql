c4224401305871d6bd55ea0fb950acd6f51e1bef
Josh Long <josh@joshlong.com>
2020-01-21 10:44:30 -0700

    polish

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,78 +1,181 @@
--- drop table if exists batch_job_execution_params;
--- drop table if exists batch_job_execution_context;
--- drop table if exists batch_step_execution_context;
--- drop table if exists batch_step_execution;
--- drop table if exists batch_job_execution;
--- drop table if exists batch_job_instance;
 --
--- drop sequence if exists batch_job_execution_seq;
--- drop sequence if exists batch_job_seq;
--- drop sequence if exists batch_step_execution_seq;
--- drop sequence if exists hibernate_sequence;
+-- PostgreSQL database dump
 --
--- drop table if exists podcast_media;
--- drop table if exists podcast_link;
--- drop table if exists media;
--- drop table if exists link;
--- drop table if exists podcast;
 
-create table if not exists link
-(
-    id          bigint not null
-        constraint link_pkey
-            primary key,
-    description varchar(255),
-    href        varchar(255)
-);
+-- Dumped from database version 11.5
+-- Dumped by pg_dump version 11.5
 
-create table if not exists media
-(
-    id          bigint not null
-        constraint media_pkey
-            primary key,
-    description varchar(255),
-    extension   varchar(255),
-    file_name   varchar(255),
-    href        varchar(255),
-    type        varchar(255)
-);
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
+--
+-- Name: hibernate_sequence; Type: SEQUENCE; Schema: public; Owner: orders
+--
 
-create table if not exists podcast
-(
-    id                  bigint not null
-        constraint podcast_pkey
-            primary key,
-    date                timestamp,
-    description         varchar(255),
-    s3_fqn_uri          varchar(255),
-    notes               varchar(255),
-    s3_output_file_name varchar(255),
-    title               varchar(255),
-    transcript          varchar(255),
-    uid                 varchar(255)
-);
+CREATE SEQUENCE  hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
-create table if not exists podcast_link
-(
-    podcast_id bigint not null
-        constraint fkllcnm8ch4ses0kchqayiylv9c
-            references podcast,
-    link_id    bigint not null
-        constraint fk2vu3w8tjdo0qb3vkpeydcc3w0
-            references link
+ALTER TABLE  hibernate_sequence OWNER TO orders;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: link; Type: TABLE; Schema: public; Owner: orders
+--
+
+CREATE TABLE  link (
+    id bigint NOT NULL,
+    description character varying(255),
+    href character varying(255)
 );
 
 
-create table if not exists podcast_media
-(
-    podcast_id bigint not null
-        constraint fkep89648nfax8u5t7cjle9bh77
-            references podcast,
-    media_id   bigint not null
-        constraint fk8g18uypwfolj3nu8jew7vj6ex
-            references media
+ALTER TABLE  link OWNER TO orders;
+
+--
+-- Name: media; Type: TABLE; Schema: public; Owner: orders
+--
+
+CREATE TABLE  media (
+    id bigint NOT NULL,
+    description character varying(255),
+    extension character varying(255),
+    file_name character varying(255),
+    href character varying(255),
+    type character varying(255)
 );
 
+
+ALTER TABLE  media OWNER TO orders;
+
+--
+-- Name: podcast; Type: TABLE; Schema: public; Owner: orders
+--
+
+CREATE TABLE  podcast (
+    id bigint NOT NULL,
+    date timestamp without time zone,
+    description character varying(255),
+    notes character varying(255),
+    podbean_draft_created timestamp without time zone,
+    podbean_draft_published timestamp without time zone,
+    podbean_media_uri character varying(255),
+    s3_audio_file_name character varying(255),
+    s3_audio_uri character varying(255),
+    s3_photo_file_name character varying(255),
+    s3_photo_uri character varying(255),
+    title character varying(255),
+    transcript character varying(255),
+    uid character varying(255)
+);
+
+
+ALTER TABLE  podcast OWNER TO orders;
+
+--
+-- Name: podcast_link; Type: TABLE; Schema: public; Owner: orders
+--
+
+CREATE TABLE  podcast_link (
+    podcast_id bigint NOT NULL,
+    link_id bigint NOT NULL
+);
+
+
+ALTER TABLE  podcast_link OWNER TO orders;
+
+--
+-- Name: podcast_media; Type: TABLE; Schema: public; Owner: orders
+--
+
+CREATE TABLE  podcast_media (
+    podcast_id bigint NOT NULL,
+    media_id bigint NOT NULL
+);
+
+
+ALTER TABLE  podcast_media OWNER TO orders;
+
+
+--
+-- Name: hibernate_sequence; Type: SEQUENCE SET; Schema: public; Owner: orders
+--
+
+SELECT pg_catalog.setval(' hibernate_sequence', 220, true);
+
+
+--
+-- Name: link link_pkey; Type: CONSTRAINT; Schema: public; Owner: orders
+--
+
+ALTER TABLE ONLY  link
+    ADD CONSTRAINT link_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: media media_pkey; Type: CONSTRAINT; Schema: public; Owner: orders
+--
+
+ALTER TABLE ONLY  media
+    ADD CONSTRAINT media_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: podcast podcast_pkey; Type: CONSTRAINT; Schema: public; Owner: orders
+--
+
+ALTER TABLE ONLY  podcast
+    ADD CONSTRAINT podcast_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: podcast_link fk2vu3w8tjdo0qb3vkpeydcc3w0; Type: FK CONSTRAINT; Schema: public; Owner: orders
+--
+
+ALTER TABLE ONLY  podcast_link
+    ADD CONSTRAINT fk2vu3w8tjdo0qb3vkpeydcc3w0 FOREIGN KEY (link_id) REFERENCES  link(id);
+
+
+--
+-- Name: podcast_media fk8g18uypwfolj3nu8jew7vj6ex; Type: FK CONSTRAINT; Schema: public; Owner: orders
+--
+
+ALTER TABLE ONLY  podcast_media
+    ADD CONSTRAINT fk8g18uypwfolj3nu8jew7vj6ex FOREIGN KEY (media_id) REFERENCES  media(id);
+
+
+--
+-- Name: podcast_media fkep89648nfax8u5t7cjle9bh77; Type: FK CONSTRAINT; Schema: public; Owner: orders
+--
+
+ALTER TABLE ONLY  podcast_media
+    ADD CONSTRAINT fkep89648nfax8u5t7cjle9bh77 FOREIGN KEY (podcast_id) REFERENCES  podcast(id);
+
+
+--
+-- Name: podcast_link fkllcnm8ch4ses0kchqayiylv9c; Type: FK CONSTRAINT; Schema: public; Owner: orders
+--
+
+ALTER TABLE ONLY  podcast_link
+    ADD CONSTRAINT fkllcnm8ch4ses0kchqayiylv9c FOREIGN KEY (podcast_id) REFERENCES  podcast(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
 
