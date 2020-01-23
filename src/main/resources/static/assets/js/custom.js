00f09bed787743cd5acb570a1f76808ccc7e3de3
Josh Long <josh@joshlong.com>
2020-01-23 12:01:13 -0800

    polish

--- src/main/resources/static/assets/js/custom.js
+++ src/main/resources/static/assets/js/custom.js
@@ -1,41 +1,31 @@
-
-
-
 $(document).ready(function () {
-
-  $('.mobile-nav-btn').on('click', function () {
-    $('.hamburger-menu').toggleClass('open');
-  });
-
+    $('.mobile-nav-btn').on('click', function () {
+        $('.hamburger-menu').toggleClass('open');
+    });
 });
 
+$(document).ready(function () {
+    $('.tab-pane-toggle').click(function (e) {
+        var id = e.currentTarget.id;
+        var contentDiv = id.split('-tab')[0] + '-content';
+        $('.tab-pane-content').removeClass('active');
+        $('#' + contentDiv).addClass('active');
+    });
+});
 
 $(document).ready(function () {
-	$('.tab-pane-toggle').click(function (e) {
-		var id = e.currentTarget.id;
-		var contentDiv = id.split ('-tab')[0] + '-content'
-		console.log ( 'the content div is ' + contentDiv)
-		$('.tab-pane-content').removeClass('active');
-		$('#' + contentDiv).addClass('active');
-	});
-})
 
+    $("#scrollTop, .btn-slide").click(function () {
+        // get the destination
+        var destination = $(this).attr('href');
 
+        $('html, body').stop().animate({
+            scrollTop: $(destination).offset().top
+        }, 700);
 
-$(document).ready(function () {
-/* var navheight = $('#navbar').height(); */
-
-	$("#scrollTop, .btn-slide").click(function() {
-	    // get the destination
-	    var destination = $(this).attr('href');
-	    
-	    $('html, body').stop().animate({
-	        scrollTop: $(destination).offset().top
-	    }, 700);
-	    
-	    // override the default link click behavior
-	    return false;
-	});
+        // override the default link click behavior
+        return false;
+    });
 
 });
 
