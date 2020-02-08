b4c2cea6698a536f62ccf6346a5dbf41d5697836
Josh Long <josh@joshlong.com>
2020-02-08 01:10:25 -0800

    script away

--- bin/rebuild-assets.sh
+++ bin/rebuild-assets.sh
@@ -3,4 +3,6 @@
 ##
 ## Run this script to re-build the minified CSS and JavaScript
 ##
-cd `dirname $0` && cd ../.. && npm install && grunt  
+cd $(dirname $0)/..
+pwd
+npm install grunt && npm install  && grunt
