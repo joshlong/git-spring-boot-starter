24243b4b565bd8685554267de59e8d8b865620c0
Josh Long <josh@joshlong.com>
2020-02-09 18:35:48 -0800

    webp or bust

--- bin/serve/serve.py
+++ bin/serve/serve.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     generation_folder = os.path.join (os.environ['PODCAST_GENERATOR_OUTPUT_DIRECTORY'], 'git-clone-of-blog')
     print(generation_folder)
     handler_class = http.server.partial(http.server.SimpleHTTPRequestHandler, directory=generation_folder)
-    http.server.test(HandlerClass=handler_class, port=9090, bind='localhost')
+    http.server.test(HandlerClass=handler_class, port=9090, bind='Joshuas-MacBook-Pro-2019.local')
